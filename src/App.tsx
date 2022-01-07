d2c4a13c77e7b884cab37f5318a19163fe539c8b
Jessica Mary <jessicamary472@gmail.com>
2022-01-07 18:05:24 +0800

    Revert "Merge pull request #1 from yehudanathan/models"
    This reverts commit ec0731224ac6da4bd1b6ba325f0fc1db2e72b58c, reversing
    changes made to e91e41a4cc018b5049d7283143f0131350ce0483.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React HELLO
+          Learn React
         </a>
       </header>
     </div>

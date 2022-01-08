ae6925485a93d73f6170cf5082e7c53461f9ade0
Edward Alvin <alvinpinter02@gmail.com>
2022-01-08 17:20:22 +0800

    minor type fix, create session API, and implement token in auth API

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { AuthUser } from "../models/AuthUser";
+import { User } from '../models/User';
 const baseServerURL = "https://qr-bookmark.herokuapp.com";
 
 export const authRegister = async (user: AuthUser) => {
@@ -17,15 +18,13 @@ export const authLogin = async (user: AuthUser) => {
   const response = await axios.post(`${baseServerURL}/login`, user);
   if (response.status === 200) console.log("User logged in successfully");
   sessionStorage.setItem("token", response.data.accessToken);
+  sessionStorage.setItem("user", JSON.stringify(response.data.user));
   return response.data;
 };
 
 export const authLogout = async () => {
     console.log("User logged out successfully");
     sessionStorage.removeItem("token");
+    sessionStorage.removeItem("user");
     return true;
-}
-
-export const getLatestToken = () => {
-    return sessionStorage.getItem("token");
-}
+};
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import {Pictures} from './Pictures'
 import { AuthUser } from './AuthUser';
 
 export interface User extends AuthUser {
+    id: number;
     email: string;
     name: string;
     password: string;

e3012f477ae584777ed8fa66daf274113fface1b
Jessica Mary <jessicamary472@gmail.com>
2022-01-08 14:39:06 +0800

    Add QR Scanner

---
+++ src/components/QRScanner.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import QrReader from "react-qr-scanner";
+import validator from "validator";
+import { Button } from "@mui/material";
+
+const QRScanner = () => {
+    const [result, setResult] = useState('No result');
+    const [showQrReader, setShowQrReader] = useState(false);
+
+    const previewStyle = {
+        height: 400,
+        width: 400,
+    }
+
+    const handleScan = (e: any) => {
+        const text: string = e === null ? 'No result' : e.text;
+        if (validator.isURL(text)) {
+            setResult(text);
+            alert("Scanner result: " + result);
+        }
+    }
+
+    const handleError = (e: Error) => {
+        console.error(e);
+    }
+
+    return (
+        <>
+            <Button
+                variant = "contained"
+                size= "large"
+                sx={{height:"40px"}}
+                onClick={() => setShowQrReader(!showQrReader)}
+                type="submit"
+                >
+                {showQrReader ? "Close" : "Open Camera"}
+            </Button>
+            {showQrReader && <QrReader
+                style={previewStyle}
+                onScan={handleScan}
+                onError={handleError}
+            />}  
+        </>
+    )
+}
+
+export default QRScanner;

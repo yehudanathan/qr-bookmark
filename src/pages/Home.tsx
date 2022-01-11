e6044d2d30eec35f655ae016dbe755d06e85de2c
Jessica Mary <jessicamary472@gmail.com>
2022-01-11 20:40:03 +0800

    Add title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ const Home = () => {
     return (
         <div className="body">
             <Stack direction="column" spacing={1} alignItems="center">
+                <h1 className="title">Bookmark your link now.</h1>
                 <AddLink/>
                 <QRScanner/>
             </Stack>

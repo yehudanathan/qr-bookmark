1a347cc843f9a97c4483bd838c3b1d65cd14caec
Jessica Mary <jessicamary472@gmail.com>
2022-01-20 20:37:04 +0800

    Create register page

--- src/components/PictGenerator.tsx
+++ src/components/PictGenerator.tsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const PictGenerator = () => {
-    const baseUnsplashURL = "https://api.unsplash.com/photos/random?client_id=CNqJperIJlMcgWD2L7jzmRD7VfA5_QcVMXpv3ELsZ-Y";
+const PictGenerator = ({ onGenerate }) => {
+    const baseUnsplashURL = "https://api.unsplash.com/photos/random?client_id=CNqJperIJlMcgWD2L7jzmRD7VfA5_QcVMXpv3ELsZ-Y&orientation=landscape";
     const [photo, setPhotoAs] = useState<any>(null);
 
     const setPhoto = async () => {
         const result = await axios.get(baseUnsplashURL);
         setPhotoAs(result);
+        onGenerate();
+        console.log("finishef");
     }
 
     useEffect(() => {
@@ -16,13 +18,6 @@ const PictGenerator = () => {
 
     if (photo === null) {
         return <div>Loading...</div>;
-    } else if (photo.errors) {
-        return (
-            <div>
-                <div>{photo.errors[0]}</div>
-                <div>Access denied</div>
-            </div>
-        );
     } else {
         return (
             <a

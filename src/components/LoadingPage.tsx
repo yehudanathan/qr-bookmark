4e13fd1606b474613d4734eacbf71aed38c4395a
Jessica Mary <jessicamary472@gmail.com>
2022-05-11 13:41:20 +0700

    Add loading animation

---
+++ src/components/LoadingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+  return (
+    <div className="parent-roller">
+       <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    </div>
+  )
+}
+
+export default LoadingPage;
--- src/components/PictGenerator.tsx
+++ src/components/PictGenerator.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import LoadingPage from "./LoadingPage";
 
 const PictGenerator = ({ onGenerate }) => {
     const baseUnsplashURL = "https://api.unsplash.com/photos/random?client_id=CNqJperIJlMcgWD2L7jzmRD7VfA5_QcVMXpv3ELsZ-Y&orientation=landscape";
@@ -16,7 +17,7 @@ const PictGenerator = ({ onGenerate }) => {
     }, [])
 
     if (photo === null) {
-        return <div>Loading...</div>;
+        return <LoadingPage/>;
     } else {
         return (
             <a

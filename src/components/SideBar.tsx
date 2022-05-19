0c925bf15f3519965f349c8834725d6d7453d136
Jessica Mary <jessicamary472@gmail.com>
2022-05-19 23:15:16 +0700

    Hot fix cursor for hamburger button

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -89,9 +89,7 @@ export default function TemporaryDrawer() {
   return (
     <div>
       <React.Fragment>
-        <button style={{background: `url("${path}") `, width: "20px", height: "20px", backgroundSize: 'cover', border: 'none'}}
-          onClick={e => toggleDrawer("left", true)}
-        ></button>
+        <button style={{background: `url("${path}") `, width: "20px", height: "20px", backgroundSize: 'cover', border: 'none', cursor: 'pointer'}} onClick={e => toggleDrawer("left", true)}></button>
         <Drawer
           anchor="left"
           open={state.left}

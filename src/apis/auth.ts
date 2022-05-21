2fc3d8f0204d80a8eb58c061cee94b69f5c03758
Jessica Mary Listijo <88389105+maryjess@users.noreply.github.com>
2022-05-21 14:21:09 +0700

    Revert "Create mock data for user"

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,13 +1,6 @@
-/* 
-NOTE: mock data is currently used in "../data/users.json".
-Cannot use authRegister while using mock data.
-Code for real data is currently commented for authLogin, isSignedIn
-*/
-
 import axios from "axios";
 import { AuthUser } from "../models/AuthUser";
 import { getCurrentUser, getLatestToken } from "./session";
-import users from "../data/users.json";
 
 const baseServerURL = "https://qr-bookmark.herokuapp.com";
 
@@ -23,33 +16,19 @@ export const authRegister = async (user: AuthUser) => {
 };
 
 export const authLogin = async (user: AuthUser) => {
-  // try {
-  //   const response = await axios.post(`${baseServerURL}/login`, user);
-  //   if (response.status === 200) console.log("User logged in successfully");
-  //   else if (response.status === 400) return ("Incorrect password");
-  //   sessionStorage.setItem("token", response.data.accessToken);
-  //   sessionStorage.setItem("user", JSON.stringify(response.data.user));
-  //   return "Logged in";
-  // } catch (error : any) {
-  //   console.log(error.response.data);
-  //   return error.response.data;
-  // }
-  console.log("loading mock data...");
-  const emails = Object.values(users["registered-users"]).map(a => a.email);
-  console.log(emails);
-  if (user.email === emails.find(u => u === user.email)) {
-    console.log("email found in mock data");
-    const passwords = Object.values(users["registered-users"]).map(us => {if (us.email === user.email) return us.password;});
-    console.log(passwords);
-    if (user.password === passwords.find(p => p === user.password)) {
-      console.log("password correct");
-      sessionStorage.setItem("token", "this-is-a-mock-token");
-      sessionStorage.setItem("user", JSON.stringify(user));
-      return "Logged in";
-    } else {
-      console.log("password incorrect");
-      return "Incorrect password";
+  try {
+    const response = await axios.post(`${baseServerURL}/login`, user);
+    if (response.status === 200) console.log("User logged in successfully");
+    else if (response.status === 400) return ("Incorrect password");
+    sessionStorage.setItem("token", response.data.accessToken);
+    sessionStorage.setItem("user", JSON.stringify(response.data.user));
+    return "Logged in";
+  } catch (error) {
+    let errorMessage = "Failed to do something exceptional";
+    if (error instanceof Error) {
+      errorMessage = error.message;
     }
+    console.log(errorMessage);
   }
 };
 
@@ -65,22 +44,20 @@ export const isSignedIn = async () => {
   const token = getLatestToken();
   const user = getCurrentUser();
   if (token && user) {
-    // try {
-    //   const response = await axios.get(`${baseServerURL}/users/${user.id}`, {
-    //     headers: {
-    //       Authorization: `Bearer ${token}`,
-    //     },
-    //   });
-    //   if (response.status === 200) {
-    //     sessionStorage.setItem("user", JSON.stringify(response.data));
-    //     return true;
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    //   return false;
-    // }
-    sessionStorage.setItem("user", JSON.stringify(user));
-    return true;
+    try {
+      const response = await axios.get(`${baseServerURL}/users/${user.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.status === 200) {
+        sessionStorage.setItem("user", JSON.stringify(response.data));
+        return true;
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
   return false;
 };
--- src/apis/session.ts
+++ src/apis/session.ts
@@ -1,9 +1,5 @@
 
 import { User } from '../models/User';
-/* 
-NOTE: mock data is currently used in "../data/users.json".
-Code for real data is currently commented for getLatestToken, getLatestUser
-*/
 
 export const getLatestToken = () => {
   return sessionStorage.getItem("token");

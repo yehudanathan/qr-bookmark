0cf5345c62624d4c76e46cabe30599ca3e80ec7f
Jessica Mary <jessicamary472@gmail.com>
2022-06-15 17:08:48 +0700

    Fix upload button for profile picture

--- src/components/EditProfile.tsx
+++ src/components/EditProfile.tsx
@@ -1,9 +1,9 @@
-import { Button, TextField } from "@mui/material";
+import { Button, IconButton, Input, TextField } from "@mui/material";
 import { Stack } from "@mui/material";
 import { useNavigate } from "react-router";
 import { useState } from "react";
 import { getUser, updateUser } from "../firebase/auth/auth_user";
-import Edit from "../static/edit.png";
+import EditIcon from '@mui/icons-material/Edit';
 
 const EditProfile = () => {
   let navigate = useNavigate();
@@ -33,17 +33,26 @@ const EditProfile = () => {
 
   return (
     <>
-    <Stack sx={{padding: 3}} alignItems="center" spacing={1.5}>
+    <Stack sx={{padding: 3, marginBottom: "10px"}} alignItems="center" spacing={1.5}>
       <h1 className="profile-h1">Configure Your Profile</h1>
+
       <div className="profile-picture-hover">
-        <button className="profile-picture-hover" onClick={handleEditPicture} style={{cursor: "pointer"}}>
-          <img className="profile-picture edit" src={"https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg"} alt="profile"/>
-        </button>
         
-        <div className="profile-picture-middle">
-          {/* <button style={{background: Edit, width: "30px", height: "30px", backgroundSize: "cover", border: "none", cursor: "pointer"}}></button> */}
-          <img src={Edit} alt="edit" width="30" height="30" style={{cursor: "pointer"}}/>
-        </div>
+        
+        <label htmlFor="icon-button-file">
+          <Button className="profile-picture-hover" component="span" sx={{borderRadius:"50%"}} >
+            <img className="profile-picture edit" src={"https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg"} alt="profile"/>
+          </Button>
+        </label>
+        
+          
+        <Input id="icon-button-file" type="file" sx={{display: "none"}}/>          
+        <label htmlFor="icon-button-file">
+          <div className="profile-picture-middle">
+            {/* <button style={{background: Edit, width: "30px", height: "30px", backgroundSize: "cover", border: "none", cursor: "pointer"}}></button> */}
+            <EditIcon sx={{fontSize: 60, cursor:"pointer"}}/>
+          </div>
+        </label>
       </div>
       
       <form className="edit-profile" onSubmit={handleSubmit}>

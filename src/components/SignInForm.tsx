f440c28cfd9f864442af8db6ae303bb819b8fd03
Jessica Mary <jessicamary472@gmail.com>
2022-06-21 22:23:17 +0700

    Add sign in with google

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,9 +1,11 @@
 import { Button, Stack, TextField } from '@mui/material';
 import { useState } from 'react';
+import GoogleButton from 'react-google-button';
 import { useNavigate } from 'react-router-dom';
 // import { authLogin } from '../apis/auth';
 // import users from "../data/users.json";
 import { emailSignIn } from '../firebase/auth/auth_email_password';
+import { googleSignIn } from '../firebase/auth/auth_google';
 
 const SignInForm = ({ isLoading }) => {
     let navigate = useNavigate();
@@ -72,17 +74,40 @@ const SignInForm = ({ isLoading }) => {
         }
     }
 
+    const handleGoogleSignIn = () => {
+        console.log("google sign in");
+        googleSignIn();
+    }
+
     if (isLoading) {
         return (<></>);
     } else {
-        return ( <>
-            <form className="form-control" onSubmit={handleSubmit}>
-                <Stack alignItems="center" spacing={3}>
-                    <Stack alignItems="center">
-                        <h1 className="subtitle"><strong>Sign in to your account.</strong></h1> 
-                        {!emailError? <TextField
+        return ( 
+        <>
+        <form className="form-control" onSubmit={handleSubmit}>
+            <Stack alignItems="center" spacing={3}>
+                <Stack alignItems="center">
+                    <h1 className="subtitle"><strong>Sign in to your account.</strong></h1>
+                    {!emailError? <TextField
+                        required
+                        label="Email"
+                        sx={{m: 1, width: "50ch", backgroundColor: "white",
+                        borderTopLeftRadius: "4px",
+                        borderTopRightRadius: "4px",
+                        }}
+                        size="small"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        color="success"
+                        inputProps={{style: {fontFamily: "Product Sans"}}}
+                        /> : 
+                        <Stack>
+                            <TextField
+                            className="error-text-field"
                             required
+                            error
                             label="Email"
+                            helperText={emailError}
                             sx={{m: 1, width: "50ch", backgroundColor: "white",
                             borderTopLeftRadius: "4px",
                             borderTopRightRadius: "4px",
@@ -90,80 +115,68 @@ const SignInForm = ({ isLoading }) => {
                             size="small"
                             value={email}
                             onChange={e => setEmail(e.target.value)}
-                            color="success"
-                            inputProps={{style: {fontFamily: "Product Sans"}}}
-                            /> : 
-                            <Stack>
-                                <TextField
-                                className="error-text-field"
-                                required
-                                error
-                                label="Email"
-                                helperText={emailError}
-                                sx={{m: 1, width: "50ch", backgroundColor: "white",
-                                borderTopLeftRadius: "4px",
-                                borderTopRightRadius: "4px",
-                                }}
-                                size="small"
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
-                                inputProps={{style: {fontFamily: "Product Sans"}}}
-                                />
-                                <div className="blank"></div>
-                            </Stack>
-                            }
-                        {!passwordError ? <TextField
-                            required
-                            type="password"
-                            label="Password"
-                            sx={{m: 1, width: "50ch", backgroundColor: "white",
-                            borderTopLeftRadius: "4px",
-                            borderTopRightRadius: "4px",
-                            }}
-                            size="small"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                            color="success"
-                            inputProps={{style: {fontFamily: "Product Sans"}}}
-                            /> : 
-                            <TextField
-                            error
-                            className="error-text-field"
-                            helperText={passwordError}
-                            required
-                            type="password"
-                            label="Password"
-                            sx={{m: 1, width: "50ch", backgroundColor: "white",
-                            borderTopLeftRadius: "4px",
-                            borderTopRightRadius: "4px",
-                            }}
-                            size="small"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                            color="success"
                             inputProps={{style: {fontFamily: "Product Sans"}}}
                             />
-                            }
-                    </Stack>
+                            <div className="blank"></div>
+                        </Stack>
+                        }
+                    {!passwordError ? <TextField
+                        required
+                        type="password"
+                        label="Password"
+                        sx={{m: 1, width: "50ch", backgroundColor: "white",
+                        borderTopLeftRadius: "4px",
+                        borderTopRightRadius: "4px",
+                        }}
+                        size="small"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        color="success"
+                        inputProps={{style: {fontFamily: "Product Sans"}}}
+                        /> : 
+                        <TextField
+                        error
+                        className="error-text-field"
+                        helperText={passwordError}
+                        required
+                        type="password"
+                        label="Password"
+                        sx={{m: 1, width: "50ch", backgroundColor: "white",
+                        borderTopLeftRadius: "4px",
+                        borderTopRightRadius: "4px",
+                        }}
+                        size="small"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        color="success"
+                        inputProps={{style: {fontFamily: "Product Sans"}}}
+                        />
+                        }
+                </Stack>
+
+                <Stack direction="row" spacing={3} style={{marginTop: "12px"}}>
+                    <GoogleButton style={{borderRadius: "2%"}} onClick={handleGoogleSignIn}/>
                     <Button
-                        style={{backgroundColor: "#019875"}}
+                        style={{backgroundColor: "#019875", borderRadius: "2%"}}
                         variant = "contained"
                         size= "large"
-                        sx={{height:"40px",
+                        sx={{height:"50px",
                             fontFamily:"Montserrat"}}
                         type="submit"
                         >
                         Sign In
                     </Button>
-                    <Stack direction="row" alignItems="center" spacing={0.5}>
-                        <h3 className="header-3">Don't have an account?</h3>
-                        <a href="/register" className="link">Register.</a>
-                    </Stack>
                 </Stack>
-            </form>
-            </>
-        )
-    }
+
+                <Stack direction="row" alignItems="center" spacing={0.5} style={{marginTop: "10px"}}>
+                    <h3 className="header-3">Don't have an account?</h3>
+                    <a href="/register" className="link">Register.</a>
+                </Stack>
+            </Stack>
+        </form>
+    </>
+    )
+}
 }
 
 export default SignInForm;

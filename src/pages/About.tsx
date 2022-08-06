7985de757d4ba09ff7e3bf049e953d8594bacf1a
Jessica Mary <jessicamary472@gmail.com>
2022-08-06 17:12:37 +0700

    Style about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -24,57 +24,60 @@ const About = () => {
     </Helmet>
 
     <div className="about-div">
-      <div className="card-config">
-        <Card sx={{
-          paddingTop: "30px",
-          paddingBottom: "30px",
-          paddingLeft: "50px",
-          paddingRight: "50px",
-          maxWidth: "800px", 
-          minWidth: "600px", 
-          width: "70%",
-        }}>
-          <Stack direction="row" alignItems="center" spacing={0.5}
-            sx={{marginBottom: "20px",
-            transform: "translateX(-5px)"
+    </div>
+
+    <div className="card-config">
+      <Card sx={{
+        paddingTop: "30px",
+        paddingBottom: "30px",
+        paddingLeft: "50px",
+        paddingRight: "50px",
+        maxWidth: "600px", 
+        minWidth: "400px", 
+        position: "absolute", 
+        top: "50px",
+        left: "25%" // TODO
+      }}>
+        <Stack direction="row" alignItems="center" spacing={0.5}
+          sx={{marginBottom: "20px",
+          transform: "translateX(-5px)"
+        }}
+        >
+          <CropFree sx={{fontSize: "40px"}}/>
+          <h2 className="about-h2">QR-Bookmark</h2>
+        </Stack>
+        <p>is a simple bookmark manager that allows user to scan QR codes containing URLs and save them. This web application was developed using React.js and Firebase.</p>
+
+        <h2 className="get-to-know">Get to know the people behind this app.</h2>
+        <Stack direction="row" spacing={0} justifyContent="space-evenly" sx={{maxWidth: "670px"}}>
+          <Stack alignItems="center">
+            <div className="zoom">
+              <img className="profile-picture about" alt="jessica-profile" src={"https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg"}></img>
+            </div>
+            <p className="p-name">Jessica Mary Listijo</p>
+          </Stack>
+
+          <Stack alignItems="center">
+            <div className="zoom">
+              <img className="profile-picture about" alt="yehuda-profile" src={"https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg"}></img>
+            </div>
+            <p className="p-name">Yehuda Nathan</p>
+          </Stack>
+
+          <Stack alignItems="center">
+            <div className="zoom">
+              <img className="profile-picture about" alt="alvin-profile" src={"https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg"}></img>
+            </div>
+            <p className="p-name">Edward Alvin</p>
+          </Stack>
+        </Stack>
+        <Button variant="contained" onClick={() => navigate('/')} startIcon={<ArrowBack/>}
+          sx={{marginTop: "60px",
+            color: "white",
+            backgroundColor: "#115e90",
           }}
-          >
-            <CropFree sx={{fontSize: "40px"}}/>
-            <h2 className="about-h2">QR-Bookmark</h2>
-          </Stack>
-          <p>is a simple bookmark manager that allows user to scan QR codes containing URLs and save them. This web application was developed using React.js and Firebase.</p>
-
-          <h2 className="get-to-know">Get to know the people behind this app.</h2>
-          <Stack direction="row" spacing={0} justifyContent="space-evenly" sx={{maxWidth: "670px"}}>
-            <Stack alignItems="center">
-              <div className="zoom">
-                <img className="profile-picture about" alt="jessica-profile" src={"https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg"}></img>
-              </div>
-              <p className="p-name">Jessica Mary Listijo</p>
-            </Stack>
-
-            <Stack alignItems="center">
-              <div className="zoom">
-                <img className="profile-picture about" alt="yehuda-profile" src={"https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg"}></img>
-              </div>
-              <p className="p-name">Yehuda Nathan</p>
-            </Stack>
-
-            <Stack alignItems="center">
-              <div className="zoom">
-                <img className="profile-picture about" alt="alvin-profile" src={"https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg"}></img>
-              </div>
-              <p className="p-name">Edward Alvin</p>
-            </Stack>
-          </Stack>
-          <Button variant="contained" onClick={() => navigate('/')} startIcon={<ArrowBack/>}
-            sx={{marginTop: "60px",
-              color: "white",
-              backgroundColor: "#115e90",
-            }}
-            >Back</Button>
-        </Card>
-      </div>
+          >Back</Button>
+      </Card>
     </div>
   </>
   )
